bf227710f610d9edaf339b3921ccd53fc1e9aeb4
James Timmins <jameshtimmins@gmail.com>
2018-01-10 22:26:10 -0800

    Add repo model.

--- repos/models.py
+++ repos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Repo(models.Model):
+
+    STATUS_ACTIVE = 'active'
+    STATUS_ERROR = 'error'
+
+    path = models.CharField(max_length=256)
+    details = models.TextField()
+    status = models.CharField(max_length=128, default=STATUS_ACTIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
