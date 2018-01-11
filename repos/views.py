ff7a26ab6df189acb14905c91423537d83bec007
James Timmins <jameshtimmins@gmail.com>
2018-01-10 22:30:40 -0800

    Add site views.

--- repos/views.py
+++ repos/views.py
@@ -1,17 +1,30 @@
+import json
+from .models import Repo
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.template.loader import render_to_string
 from services import queue, repo
 
 
 def home(request):
+    """
+    LOCode home page.
+    """
     return render(request, 'repos/home.html')
 
 
 def search(request):
+    """
+    Search for repo data and queue if not found.
+    """
     validated_repo = repo.validate(request.GET['route'])
-    if validated_repo:
-        message = 'success {}'.format(validated_repo)
-        queue.send_message(validated_repo)
-    else:
-        message = 'bad repo'
-    return JsonResponse({'success': message})
+    if not validated_repo:
+        return JsonResponse({'status': 'invalid'})
+
+    try:
+        result = Repo.objects.get(path=validated_repo)
+        render = render_to_string("repos/results.html", {'results': json.loads(result.details)})
+        return HttpResponse(render)
+    except Repo.DoesNotExist:
+        queue.queue_repo(validated_repo)
+        return JsonResponse({'status': 'queued'})
