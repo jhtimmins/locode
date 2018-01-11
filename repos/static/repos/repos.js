82d7d933ed588eb927a84e088959751d3e471aac
James Timmins <jameshtimmins@gmail.com>
2018-01-10 22:26:47 -0800

    Remove test code.

--- repos/static/repos/repos.js
+++ repos/static/repos/repos.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
   $("#search").on('click', function() {
     var repo = $("#search-field").val()
-    $.get('/repos/search?route=' + repo, function(data, status) {
-      console.log(data);
+    $.get('/repos/search?route=' + repo, function(data) {
+        $("#results").html(data);
     })
   })
 });
