b12ee90eed814e9ae8273d0dea5799b71f795afd
James Timmins <jameshtimmins@gmail.com>
2018-01-10 22:25:13 -0800

    Move passwords to config.

--- locode/settings.py
+++ locode/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+import config
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')dc4693cooycno=&nvfz*rkeisu#ykr7gb0@pqxxikh=eu^fm('
+SECRET_KEY = config.django['secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'locode.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.database['name'],
+        'USER': config.database['user'],
+        'PASSWORD': config.database['password'],
+        'HOST': config.database['host'],
+        'PORT': config.database['port'],
     }
 }
 
